5018fd9fbbc7c4d05694e5cc72f4f7e85ec63147
Fatima Fayyaz <fatimafayyaz206@gmail.com>
2024-11-24 22:04:28 +0500

    Set Environment Variables

--- main.py
+++ main.py
@@ -2,4 +2,12 @@ import requests
 import smtplib
 import os
 
+MY_LAT = os.environ.get("MY_LAT")
+MY_LON = os.environ.get("MY_LON")
+MY_EMAIL = os.environ.get("MY_EMAIL")
+MY_PASSWORD = os.environ.get("MY_PASSWORD")
+
+OWM_endpoint = "https://api.openweathermap.org/data/2.8/onecall"
+API_KEY = os.environ.get("API_KEY")
+
 
