4e19624a676cce7e9683520d2e83ad8224cb85b3
Fatima Fayyaz <fatimafayyaz206@gmail.com>
2024-11-24 23:52:41 +0500

    Update main.py

--- main.py
+++ main.py
@@ -2,25 +2,31 @@ import requests
 import smtplib
 import os
 
+#current latitude and longitude
 MY_LAT = os.environ.get("MY_LAT")
 MY_LON = os.environ.get("MY_LON")
+
+#email id and password for login
 MY_EMAIL = os.environ.get("MY_EMAIL")
 MY_PASSWORD = os.environ.get("MY_PASSWORD")
 
 OWM_endpoint = "https://api.openweathermap.org/data/2.8/onecall"
 API_KEY = os.environ.get("API_KEY")
 
+#OWM parameters 
 parameters = {
     "lat": MY_LAT,
     "lon": MY_LON,
     "appid": API_KEY,
     "exclude": "current,minutely,daily,alerts",
 }
+
+#get weather data
 response = requests.get(url=OWM_endpoint, params=parameters)
 response.raise_for_status()
 weather_data = response.json()
 
-
+#send email
 for hour in weather_data["hourly"][:12]:
     weather_code = hour["weather"][0]["id"]
     if weather_code < 700:
